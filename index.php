f435e2ea650997d5fdbdc9a0f4853fa72032fb20
Victor Arango <varangoquiro@umass.edu>
2017-11-19 22:32:25 -0500

    now index.php can upload files to S3 server

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+$s3 = Aws\S3\S3Client::factory(array(
+			'signature' => 'v4',
+			'version' => 'latest',
+			'region'  => 'us-east-2'));
+
+$bucket = getenv('BUCKET')?: die('No "S3_BUCKET" config var in found in env!'); //S3_BUCKET --> BUCKET
+var_dump($bucket);
 ?>
 <html>
     <head><meta charset="UTF-8"></head>
@@ -12,7 +17,7 @@ $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!'
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo
     try {
-        $upload = $s3->upload($bucket, $_FILES['userfile'][$_POST["file_name"]], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
 ?>
         <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
 <?php } catch(Exception $e) { ?>
