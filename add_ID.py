c5bc507ab903b8500866a9bf81da05cf428a0b24
Victor Arango <varangoquiro@umass.edu>
2017-11-20 20:00:40 -0500

    Uploaded image from Python script. Checking response...

--- add_ID.py
+++ add_ID.py
@@ -1,9 +1,13 @@
 import requests
-#from PIL import Image
+import sys
+
+'''
+ Image name should be pass through console i.e sys.argv[1]
+'''
 
 # Path variables
 path="C:/Users/Victor/Documents/UMass Amherst/Fall 2017/SDP/python/" #"/home/pi/Documents/access/camera/test1.jpg"
-image_name="test.jpg"
+image_name=sys.argv[1] #"test.jpg"
 final_path=path+image_name
 # Load image:
 #img = Image.open(final_path)
@@ -11,17 +15,9 @@ img = open(final_path,'rb')
 
 base_url="https://sdp-lh18.herokuapp.com"
 final_url=base_url+"/upload.php"
-'''FIRST TRY: SUCCESSFUL
-final_url=base_url+"/index_python_access_test.php"
-payload = {'file_name': "from Python!"}
-response = requests.post(final_url, data=payload)
-
-print(response.text) #TEXT/HTML
-print(response.status_code, response.reason) #HTTP
-'''
 
 with open(final_path, 'rb') as f: response = requests.post(final_url, files={'userfile': f})
 print(response.text) #TEXT/HTML
-if response.text is "Upload successful!":
+if response.text is "Upload successful":
     print("We got response back. File is uploaded!")
 print(response.status_code, response.reason) #HTTP
