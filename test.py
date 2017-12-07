67c261aa43440e42b87b580c3d8596dfd8533381
Hassaan <hqkhan@umass.edu>
2017-12-07 00:36:49 -0500

    new changes

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import sys
 from PIL import Image
 import urllib.request
 import io
+from skimage import io
 
 #from skimage import io
 def template_matching(img, template):  
@@ -18,14 +19,14 @@ def read_image_server(URL):
     hdr = {'User-Agent': 'Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
     
-    #f = io.imread(URL, headers=hdr)
-    fd = urllib.request.urlopen(URL)
-    image_file = io.BytesIO(fd.read())
-    f = Image.open(image_file)
+    f = io.imread(URL, headers=hdr)
+    #fd = urllib.request.urlopen(URL)
+    #image_file = io.BytesIO(fd.read())
+    #f = Image.open(image_file)
     return cv2.cvtColor(f, cv2.COLOR_RGB2BGR)
 
 def main():
-    #img = read_image_server(sys.argv[1])
+    img = read_image_server(sys.argv[1])
     template = read_image_server(sys.argv[2])
     print(sys.argv[2]) 
     return 0
