ada3afedeecef9c1633e16e3ba375a1620432bf6
Hassaan <hqkhan@umass.edu>
2017-12-07 00:12:36 -0500

    new changes

--- test.py
+++ test.py
@@ -1,21 +1,14 @@
 import cv2
 import sys
-from skimage import io
+#from skimage import io
+from PIL import Image
+import urllib2 as urllib
+import io
 
-def template_matching(img, template):
-    """
-    Method to find template matching. Using OpenCV's function to do it. Finds
-    highest correlation point in the image when convolved with the template
-    and creates a bounding rectangle around the point of the template's size.
-    
-    Inputs:
-    img - original image where we need to find template in
-    template - template that is given to cv2.matchTemplate
-    """
-    
+def template_matching(img, template):  
     res = cv2.matchTemplate(img, template, cv2.TM_SQDIFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    #return min_val, min_loc
+  
     if min_val < 0.1:
         print ('Yes')
     else:
@@ -25,7 +18,10 @@ def read_image_server(URL):
     hdr = {'User-Agent': 'Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
     
-    f = io.imread(URL, headers=hdr)
+    #f = io.imread(URL, headers=hdr)
+	fd = urllib.urlopen(URL)
+	image_file = io.BytesIO(fd.read())
+	f = Image.open(image_file)
     return cv2.cvtColor(f, cv2.COLOR_RGB2BGR)
 
 def main():
