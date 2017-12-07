12b48711a212285e59c896049201a26641cc6f11
Hassaan <hqkhan@umass.edu>
2017-12-07 00:54:42 -0500

    new changes

--- test.py
+++ test.py
@@ -26,11 +26,11 @@ def read_image_server(URL):
     return cv2.cvtColor(f, cv2.COLOR_RGB2BGR)
 
 def main():
-#    img = read_image_server(sys.argv[1])
-    #template = read_image_server(sys.argv[2])
-    print(sys.argv[1], sys.argv[2]) 
-    return 0
-    #template_matching(img, template)
+    img = read_image_server(sys.argv[1])
+    template = read_image_server(sys.argv[2])
+    #print(sys.argv[1], sys.argv[2]) 
+    #return 0
+    template_matching(img, template)
 
 if __name__ == '__main__':
     main()
