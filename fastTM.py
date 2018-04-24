5ff01a81ff3fc2e4a50a3f249139efa5dbf54fe8
Hassaan <hqkhan@umass.edu>
2018-04-24 02:41:03 -0400

    6000, 3000

--- fastTM.py
+++ fastTM.py
@@ -136,8 +136,8 @@ def fast_template_match(im, template, centers, rotations, correlation_f=True):
 
 def correlation_fast_pieces_main(im, template, t_mats):
     samples = np.where(template == 255)
-    divisor = 2000
-    limit = 8000
+    divisor = 3000
+    limit = 6000
     if len(samples[0]) >= limit:
         
         # Limit to 8000 white pixels
